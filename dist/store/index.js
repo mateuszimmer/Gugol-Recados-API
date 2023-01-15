e8a4e2bf2fc331f24387fa9542b5d2cc57d6c232
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-01-15 00:55:35 -0300

    Criação user teste

--- dist/store/index.js
+++ dist/store/index.js
@@ -1,7 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.storeUsuariosLogados = exports.storeRecados = exports.storeUsuarios = void 0;
-exports.storeUsuarios = [];
-exports.storeRecados = [];
+exports.storeUsuarios = [{
+        email: 'admin@admin.com',
+        senha: 'admin'
+    }];
+exports.storeRecados = [
+    {
+        id: "12708228-d201-4e63-ad1e-4742254ec775",
+        titulo: "Titulo Admin 1",
+        descricao: "Descrição Admin 1",
+        data: '15/01/2023',
+        usuario: 'admin@admin.com',
+        visivel: true,
+        ativo: true
+    },
+    {
+        id: "b3d1088a-3669-461d-9a04-6d57507178f0",
+        titulo: "Titulo Admin 2",
+        descricao: "Descrição Admin 2",
+        data: '15/01/2023',
+        usuario: 'admin@admin.com',
+        visivel: true,
+        ativo: true
+    },
+];
 exports.storeUsuariosLogados = [];
 //# sourceMappingURL=index.js.map
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,29 @@ import { IRecado } from "../interfaces/IRecado";
 import { IUserLogado } from "../interfaces/IUserLogado";
 import { IUsuario } from "../interfaces/IUsuario";
 
-export const storeUsuarios: Array<IUsuario> = []
-export const storeRecados: Array<IRecado> = []
+export const storeUsuarios: Array<IUsuario> = [{
+    email: 'admin@admin.com',
+    senha: 'admin'
+}]
+export const storeRecados: Array<IRecado> = [
+    {
+        id: "12708228-d201-4e63-ad1e-4742254ec775",
+        titulo: "Titulo Admin 1",
+        descricao: "Descrição Admin 1",
+        data: '15/01/2023',
+        usuario: 'admin@admin.com',
+        visivel: true,
+        ativo: true
+    },
+    {
+        id: "b3d1088a-3669-461d-9a04-6d57507178f0",
+        titulo: "Titulo Admin 2",
+        descricao: "Descrição Admin 2",
+        data: '15/01/2023',
+        usuario: 'admin@admin.com',
+        visivel: true,
+        ativo: true
+    },
+    
+]
 export const storeUsuariosLogados: Array<IUserLogado> = []
